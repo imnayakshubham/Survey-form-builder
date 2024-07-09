03a89ddad83a9459aca7550bb41b744b24232fda
imnayakshubham <imnayakshubham@gmail.com>
2024-07-09 11:01:13 +0530

    feat: Added React Router Dom and updated the UI and refactor the code

---
+++ src/constant/index.js
@@ -0,0 +1,27 @@
+export const surveys = [
+    {
+        "name": "Rana Cannon",
+        "guid": "714A67C7-5686-1B8F-A130-522C90ED3DB7",
+        "company": "Nec Euismod In Corp."
+    },
+    {
+        "name": "Theodore Beck",
+        "guid": "6C237E44-45BD-ADDC-7055-50A01FC6156E",
+        "company": "Ut Institute"
+    },
+    {
+        "name": "Kenyon Yates",
+        "guid": "07A698EA-741C-4892-60E7-B12D1635D2FE",
+        "company": "Non Sollicitudin LLC"
+    },
+    {
+        "name": "Berk Terrell",
+        "guid": "2283752E-19C5-1189-7146-D6D231750379",
+        "company": "Ut Mi Company"
+    },
+    {
+        "name": "Dane Haney",
+        "guid": "5E26A243-E5BD-F7A1-234D-26F1322E4571",
+        "company": "Mollis Duis Sit LLC"
+    }
+]
